99158f5adbf9fa4e68bf9a84bea5321faa7e18f1
AntonioNtV <antoniontv18@gmail.com>
2020-10-02 22:50:00 -0300

    Student data

---
+++ src/data/StudentsData.ts
@@ -0,0 +1,19 @@
+const students: IStudent[] = [
+    {
+        description: 'Software Engineer',
+        name: 'Antonio',
+        surname: 'Bertino'
+    },
+    {
+        description: 'Frontend Developer',
+        name: 'Natália',
+        surname: 'Salvino'
+    },
+    {
+        description: 'FullStack Developer',
+        name: 'Davi',
+        surname: 'Sousa'
+    }
+]
+
+export { students }
